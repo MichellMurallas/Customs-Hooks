2fa866aa346c494faf7d266d97622f56906d2998
MichellMurallas <michellsms893@gmail.com>
2022-12-10 10:32:48 -0500

    onChange en el formulario

--- src/Components/MiFormulario.jsx
+++ src/Components/MiFormulario.jsx
@@ -25,21 +25,30 @@ const MiFormulario = () => {
     let curso = serializarFormulario(e.target)
     setFormulario(curso)
     // serializarFormulario(e.target)
-    console.log(curso)
   }
+
+  const cambiado = ({target}) =>{
+    const {name, value} = target;
+
+    setFormulario({
+      ...formulario,
+      [name]: value
+    });
+}
+
   return (
     <div>
       <h1>Formulario</h1>
       <p>Formulario para guardar curso</p>
-      <p>Curso guardado: </p>
-      <pre>{JSON.stringify(formulario)}</pre>
+      <p>Curso guardado:{ formulario.titulo} </p>
+      <pre className='codigo'>{JSON.stringify(formulario)}</pre>
 
       <form onSubmit={enviado} className="mi-formulario">
-        <input type="text" name="titulo" placeholder="Titulo" />
-        <input type="number" name="anio" placeholder="Año de publicacion:" />
-        <textarea name="descripcion" placeholder="Descripcion"/>
-        <input type="text" name="autor" placeholder="Autor" />
-        <input type="email" name="email" placeholder="Correo de contacto:" />
+        <input type="text" name="titulo" onChange={cambiado} placeholder="Titulo" />
+        <input type="number" name="anio" onChange={cambiado} placeholder="Año de publicacion:" />
+        <textarea name="descripcion" onChange={cambiado} placeholder="Descripcion"/>
+        <input type="text" name="autor" onChange={cambiado} placeholder="Autor" />
+        <input type="email" name="email" onChange={cambiado} placeholder="Correo de contacto:" />
         <input type="submit" value="Enviar" />
       </form>
     </div>
